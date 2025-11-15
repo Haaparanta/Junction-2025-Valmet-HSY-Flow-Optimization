9b0cc1233a43ca860649b312a6d0df896b71179f
Niilo Jaakkola <niilo.jaakkola@icloud.com>
2025-11-15 13:19:41 +0200

    initial frontend

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="dashboard-gradient min-h-screen p-8">
+	<div class="mx-auto max-w-7xl">
+		{@render children()}
+	</div>
+</div>
